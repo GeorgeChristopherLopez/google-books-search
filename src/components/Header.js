e62ec92f82756154ea268eafdadf277b484d68f5
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2020-07-25 20:51:28 -0400

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,50 @@
-import React from 'react';
-import { Link} from "react-router-dom";
-  
-import '../App.css';
 
-function App() {
+import { Link} from "react-router-dom";
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+
+
+function Header() {
+    /* SENSITIVE INFO ***************************************************************************************************************
+  remove before uploading */
+  const API_KEY = 'api_key';
+  /***********************************************************************************************************************************/
+
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [input, setInput] = React.useState("");
+  const [results, setResults] = React.useState([]);
+
+  const fetchData =  () => {
+      fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${API_KEY}`)
+      .then(response => response.json())
+      .then(responseData => {
+        let newResult = responseData.items;
+       
+        setResults(newResult);
+        console.log(newResult);
+      
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+  };
+ 
+ 
+  
+  const handleChange = event => {
+    setInput(event.target.value);
+    let term = (event.target.value).split(" ");
+    term = term.join('+');
+    setSearchTerm(term);
+   
+  };
+
+
+  const handleSearch = event => {
+   console.log("searching for...."+searchTerm);
+   fetchData();
+  }
+    
   return (
    
       <header className="App-header">
@@ -12,13 +53,31 @@ function App() {
         
         </Link></h1> 
        <div>
-       <input className="search-bar"></input><Link className="search-btn" to="/search">Search</Link>
+       <input 
+       className="search-bar" 
+       type="text"
+       placeholder="Search"
+       value={input}
+       onChange={handleChange}/><Link onClick={handleSearch} className="search-btn" to="/search">?</Link>
        </div>
     <p>Search the world's most comprehensive index of full-text books.</p>
     <a href="/">My library</a>
-          </header>
+ <div> 
+ <ul>
 
+{results.map(function(item, key) {
+      return <li key={key}>{item.volumeInfo.title}</li>;
+    })}
+  
+
+
+
+  </ul>
+
+ </div>
+
+          </header>
   );
 }
 
-export default App;
+export default Header;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+/* remove before uploading */
+const APIKEY = 'AIzaSyDUON-mFOfHhu8iRH585Kfjx_Sv-ZkMznI';
+const keyParam = 'key=API_KEY';
+/************************/
+/*
+fetch('https://www.googleapis.com/books/v1/volumes?q=head+first+javascript&key=AIzaSyDUON-mFOfHhu8iRH585Kfjx_Sv-ZkMznI')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+*/
 ReactDOM.render(
   <React.StrictMode>
     <App />
